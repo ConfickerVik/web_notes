e6c1277e7228a5580ee720db4cf47fa1e58588bd
confickervik <VPuRS*lodx854321>
2021-06-11 00:34:24 +0800

    create logo_page

--- web_notes/settings.py
+++ web_notes/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -119,11 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = BASE_DIR / "static"
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_DIR = BASE_DIR.joinpath("static")
+STATICFILES_DIRS = [
+                        STATIC_DIR
+                    ]
 
 MEDIA_URL = '/media/'
-MEDIA_DIR = BASE_DIR / "media"
+MEDIA_DIR = BASE_DIR.joinpath("media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
